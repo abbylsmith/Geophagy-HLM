95dd1366af3eed1cfabe6fae22a9c8a39c8f5ba0
abbylsmith <als1@u.northwestern.edu>
2018-07-26 17:05:14 -0500

    merging mice + hmi still a bit messy

--- imputations.R
+++ imputations.R
@@ -1,11 +1,11 @@
 #necessary libraries
 require(data.table)
 require(readxl)
+require(plyr)
 
 #imputation packages utilized
 require(mice)
-####SHOULD CHANGE THIS PATH SO THAT THE FILE IS PULLED FROM THE GITHUB FOLDER AND NOT FROM YOUR PERSONAL DESKTOP####
-install.packages("C:/Users/Acer/Documents/hmi_0.9.8.zip", repos = NULL, type = "source")
+install.packages("/hmi_0.9.8.zip", repos = NULL, type = "source")
 library("hmi")
 
 #######HMI PACKAGE IMPUTATIONS#######
@@ -13,112 +13,84 @@ library("hmi")
 pica_df <- data.frame(read_excel("Pica_Longitudinal_2018_02_15_kf.xlsx"))
 
 # we center all time-variant variables
-##I THINK THESE NEED TO BE GROUP MEAN CENTERED SINCE THEY ARE LEVEL 1 PREDICTORS##
-pica_df$hemo_c <- pica_df$hemo - mean(pica_df$hemo,na.rm = T)
-pica_df$fepill_c <- pica_df$fepill - mean(pica_df$fepill,na.rm = T)
-pica_df$fefood_c <- pica_df$fefood - mean(pica_df$fefood,na.rm = T)
-pica_df$fefood_yn_c <- pica_df$fefood_yn - mean(pica_df$fefood_yn, na.rm = T)
-pica_df$socialstat_comm_c <- pica_df$socialstat_comm - mean(pica_df$socialstat_comm, na.rm = T)
-pica_df$socialstat_country_c <- pica_df$socialstat_country - mean(pica_df$socialstat_country, na.rm = T)
-pica_df$depscore_c <- pica_df$depscore - mean(pica_df$depscore, na.rm = T)
-pica_df$dep_cat_c <- pica_df$dep_cat - mean(pica_df$dep_cat, na.rm = T)
-pica_df$stress_c <- pica_df$stress - mean(pica_df$stress, na.rm = T)
-pica_df$socialsupp_c <- pica_df$socialsupp - mean(pica_df$socialsupp, na.rm = T)
-pica_df$primary_wat_c <- pica_df$primary_wat - mean(pica_df$primary_wat, na.rm = T)
-pica_df$wat_improve_c <- pica_df$wat_improve - mean(pica_df$wat_improve, na.rm = T)
-pica_df$wat_tx_c <- pica_df$wat_tx - mean(pica_df$wat_tx, na.rm = T)
-pica_df$season_c <- pica_df$season - mean(pica_df$season, na.rm = T)
-pica_df$season_drywet_c <- pica_df$season_drywet - mean(pica_df$season_drywet,na.rm = T)
-pica_df$season_score_c <- pica_df$season_score - mean(pica_df$season_score,na.rm = T)
-pica_df$preg_c <- pica_df$preg-mean(pica_df$preg,na.rm=T)
-pica_df$geo_c <- pica_df$geo - mean(pica_df$geo, na.rm = T)
-pica_df$amylo_c <- pica_df$amylo - mean(pica_df$amylo, na.rm = T)
-pica_df$other_c <- pica_df$other - mean(pica_df$other, na.rm = T)
-pica_df$pica_yn_c <- pica_df$pica_yn - mean(pica_df$pica_yn, na.rm = T)
-pica_df$fias_c <- pica_df$fias - mean(pica_df$fias, na.rm = T)
-pica_df$fi_tert_c <- pica_df$fi_tert - mean(pica_df$fi_tert, na.rm = T)
-pica_df$hhs_c <- pica_df$hhs - mean(pica_df$hhs, na.rm = T)
-pica_df$hemo_c <- pica_df$hemo - mean(pica_df$hemo, na.rm = T)
-pica_df$anemic_c <- pica_df$anemic - mean(pica_df$anemic, na.rm = T)
-pica_df$nausea_c <- pica_df$nausea - mean(pica_df$nausea, na.rm = T)
-pica_df$diar_c <- pica_df$diar - mean(pica_df$diar, na.rm = T)
-pica_df$gidis_yn_c <- pica_df$gidis_yn - mean(pica_df$gidis_yn, na.rm = T)
+variables_to_center<-c("hemo", "fepill", "fefood", "fefood_yn", "socialstat_comm", 
+                       "socialstat_country", "depscore", "dep_cat", "stress", "socialsupp",
+                       "primary_wat", "wat_improve", "wat_tx", "season", "season_drywet",
+                       "season_score", "preg", "geo", "amylo", "other", "pica_yn",
+                       "fias", "fi_tert", "hhs", "nausea", "anemic", "diar", "gidis_yn")
 
-
+pica_df[,variables_to_center] <- apply(pica_df[, variables_to_center], 2, function(x) x - mean(x, na.rm = T))
 pica_df$pid <- as.factor(pica_df$pid)
-list_of_types <- list_of_types_maker(pica_df[,c('geo', 'pid','hemo_c', 'amylo_c', 'hhs_c', 'stress_c',
-                                             'preg_c' , 'diar_c','nausea_c', 'asset_index',
-                                             'other_c', 'fefood_c', 'posneg', 'depscore_c')])
+list_of_types <- list_of_types_maker(pica_df[,c('geo', 'pid','hemo', 'amylo', 'hhs', 'stress',
+                                             'preg' , 'diar','nausea', 'asset_index',
+                                             'other', 'fefood', 'posneg', 'depscore')])
 
 #coding of semi-continous variables isn't supported yet by hmi package
-list_of_types$hhs_c <- "cont"
-list_of_types$stress_c <- "cont"
+list_of_types$hhs <- "cont"
+list_of_types$stress<- "cont"
 list_of_types$asset_index <- "cont"
-list_of_types$fefood_c <- "cont"
+list_of_types$fefood <- "cont"
 
-final_formula<-geo~1+hemo_c+amylo_c + hhs_c + stress_c + preg_c + diar_c + 
-  nausea_c + asset_index + other_c + fefood_c  + posneg + depscore_c + (1|pid)
+final_formula<-geo~1+hemo +amylo + hhs + stress + preg + diar + 
+  nausea + asset_index + other + fefood  + posneg + depscore + (1|pid)
 set.seed(1)
 
 ##NOTE: This takes a long time to run! 
-hmi_imp <- hmi(data = pica_df[,c('geo','pid', 'hemo_c', 'amylo_c', 'hhs_c', 'stress_c',
-                             'preg_c' , 'diar_c','nausea_c', 'asset_index',
-                             'other_c', 'fefood_c', 'posneg', 'depscore_c')],
+hmi_imp <- hmi(data = pica_df[,c('geo','pid', 'hemo', 'amylo', 'hhs', 'stress',
+                             'preg' , 'diar','nausea', 'asset_index',
+                             'other', 'fefood', 'posneg', 'depscore')],
              model_formula = as.formula(final_formula),list_of_types = list_of_types, M = 10,
              nitt = 100000, maxit = 10)
 
 
 #######ADDING DERIVED VARIABLES TO HMI IMPUTATIONS#######
 
-##NEED TO CHANGE THE NAMES HERE TO BE CONSISTENT. geo_final SHOULD BE THE SAME THING AS HMI_IMP ABOVE?
-##SHOULD CHANGE NAME OF "final_big_imp_hmi.rds" TO JUST "final_imp_hmi.rds" 
+#the above takes a long time to run! 
 geo_final <- readRDS("final_big_imp_hmi.rds")
 
 #checks of model convergence
 bwplot(geo_final)
 densityplot(geo_final)
 
-geo_complete <- mice::complete(geo_final,"long",inc=TRUE)
-colnames(geo_complete)[c(4,5,6,7,9:12,14,18)] <- c("hemo", "amylo", "hhs", "stress", "fefood", "preg",
-                                "nausea", "diar", "depscore", "other")
+geo_complete <- mice::complete(geo_final,"long",inc=TRUE)[c(1,4,5,6,7,9:12,14,18, 19)]
+colnames(geo_complete) <- c(".imp","hemo", "amylo", "hhs", "stress", "fefood", "preg",
+                                "nausea", "diar", "depscore", "other", "pid")
+# 
+# ##If you want to add in derived variables based on the hmi imputations
+# geo_complete$visit <- rep(1:9, times = 4081) #4081 women in our dataset
+# geo_complete$pica_yn <- ifelse((geo_complete$geo == 1 | geo_complete$amylo == 1 | 
+#                                   geo_complete$other == 1),1,0)
+# geo_complete$anemic <- ifelse((geo_complete$preg == 1 & geo_complete$hemo < 11) | 
+#                                 (geo_complete$preg == 0 & geo_complete$hemo < 12),1,0)
+# geo_complete$dep_cat <- ifelse((geo_complete$depscore > 16),1,0)
+# geo_complete$gidis_yn <- ifelse((geo_complete$nausea == 1 | geo_complete$diar == 1),1,0)
+# geo_complete$fefood_yn <- ifelse((geo_complete$fefood > 0),1,0)
+# 
+# 
+#geo_complete <- split(geo_complete,geo_complete$.imp)
+# add_wealth <- function(subset_imp){
+#   subset_imp <- subset_imp %>% group_by(pid) %>% mutate(hemo_avg=(mean(hemo,na.rm = T)))
+#   subset_imp$wealth <- ifelse(subset_imp$asset_index > quantile(subset_imp$asset_index, 
+#                                                                 probs = seq(0,1,1/3),na.rm = T)[3],3,
+#                               ifelse(subset_imp$asset_index >= quantile(subset_imp$asset_index, 
+#                                                                   probs = seq(0,1,1/3),na.rm = T)[2],2,1))
+#   return(subset_imp)
+# }
 
-#adding derived variables 
-geo_complete$visit <- rep(1:9, times = 4081) #4081 women in our dataset
-geo_complete$pica_yn <- ifelse((geo_complete$geo == 1 | geo_complete$amylo == 1 | 
-                                  geo_complete$other == 1),1,0)
-geo_complete$anemic <- ifelse((geo_complete$preg == 1 & geo_complete$hemo < 11) | 
-                                (geo_complete$preg == 0 & geo_complete$hemo < 12),1,0)
-geo_complete$dep_cat <- ifelse((geo_complete$depscore > 16),1,0)
-geo_complete$gidis_yn <- ifelse((geo_complete$nausea == 1 | geo_complete$diar == 1),1,0)
-geo_complete$fefood_yn <- ifelse((geo_complete$fefood > 0),1,0)
-
-
-geo_complete <- split(geo_complete,geo_complete$.imp)
-add_wealth <- function(subset_imp){
-  ##WHY DO WE CREATE hemo_avg INSIDE THIS add_wealth() FUNCTION?
-  subset_imp <- subset_imp %>% group_by(pid) %>% mutate(hemo_avg=(mean(hemo,na.rm = T)))
-  subset_imp$wealth <- ifelse(subset_imp$asset_index > quantile(subset_imp$asset_index, 
-                                                                probs = seq(0,1,1/3),na.rm = T)[3],3,
-                              ifelse(subset_imp$asset_index >= quantile(subset_imp$asset_index, 
-                                                                  probs = seq(0,1,1/3),na.rm = T)[2],2,1))
-  return(subset_imp)
-}
-
-geo_complete_list <- lapply(geo_complete, FUN = add_wealth)
-geo_complete <- rbindlist(geo_complete_list) #using function from data.table
+# geo_complete_list <- lapply(geo_complete, FUN = add_wealth)
+# geo_complete <- rbindlist(geo_complete_list) #using function from data.table
 
 
 
 #######ADDING MICE IMPUATIONS##### 
 
-##KATIE: Can you make sure the predictor matrix includes:amylo_child", "fias", "geo_child", "gravidity", "hhloc", "other_child", 
-#"parity", "religion", "site", "tribe", "anemic", "dep_cat", "fefood_yn", "fi_tert", "gidis_yn", "pica_child", "pica_yn", "wealth"
-
-##THE NEW FILE "prediction_matrix.xlsx" HAS ALL THE VARIABLES YOU MENTION UP TO TRIBE, STARTING WITH ANEMIC THE REST ARE DERIVED SO THEY ARE ADDED IN AFTER THE IMPUTATIONS
-
 prediction_matrix <- as.matrix(data.frame(read_excel("prediction_matrix.xlsx")))
+prediction_matrix<-prediction_matrix[,-1] #first column unnecessary
 rownames(prediction_matrix) <- prediction_matrix[,1]
 
+
+pica_df <- data.frame(read_excel("Pica_Longitudinal_2018_02_15_kf.xlsx"))
+pica_df$pid<-factor(pica_df$pid)
 # Add a variable for how many times the id*cat combination has occured
 tmp <- ddply(pica_df, .(pid, visit), transform, newid = paste(pid, seq_along(visit)))
 
@@ -146,13 +118,13 @@ to_remove <- c("visit","fepill","hemo","socialstat_comm","socialstat_country",
              "dep_cat","primary_wat","wat_improve","wat_tx","nausea", "diar", "gidis_yn", 
              "anemic","season","season_drywet","season_score","geo","amylo","other","fias",
              "fi_tert","hhs", "anemic","idate","gestation","fefood_yn")
-final_pica_df_wide<-merge(pica_df_wide, subset(pica_df[!duplicated(pica_df$pid),],
+final_pica_df_wide <- merge(pica_df_wide, subset(pica_df[!duplicated(pica_df$pid),],
                                                select = colnames(pica_df) %ni% to_remove))
 
 #add last visit for each woman
 split_pica_by_pid <- split(pica_df, pica_df$pid)
 last_visit <- lapply(split_pica_by_pid, function(x) max(x$visit))
-last_visit <- data.frame(pid = as.numeric(names(last_visit)), last_visit = unlist(last_visit))
+last_visit <- data.frame(pid = as.factor(names(last_visit)), last_visit = unlist(last_visit))
 final_pica_df_wide <- merge(final_pica_df_wide, last_visit,by = 'pid')
 
 #make categorical variables factors
@@ -177,32 +149,30 @@ densityplot(imp)
 plot(imp)
 
 #save completed from MICE
-complete <- mice::complete(imp,"long",inc = TRUE) 
+complete <- mice::complete(imp,"long",include= TRUE) 
 
-#need hhs??!?!?  amylo_child?!?
 patterns <- c("hemo", "preg", "fefood", "stress", "depscore", "nausea", 
-            "diar", "fias", "geo_child", "amylo_child")
-matches <- unique(grep(paste(patterns, collapse="|"), colnames(complete), value=T))
-full_time_variant <- reshape(
-    complete[, c(".imp", ".id", "pid", matches)],
-    idvar = 'pid',
-    varying = c(4:75),
-    v.names = patterns,
-    new.row.names = 1:(nrow(complete) * 9),
-    sep = "_",
-    direction = 'long'
-  )
+            "diar", "fias")
+matches <- grep(paste(patterns, collapse="|"), colnames(complete), value=T)
+complete_wide<-complete[, c(".id", ".imp", "pid", matches)]
 
-colname_length<-length(colnames(complete))
-full_time_invariant<-complete[,c(1,3, (colname_length- 12):colname_length)]
+full_time_variant <- list()
+for(i in 1:length(patterns)){
+  full_time_variant[[i]]<-reshape(complete_wide[, c(".id", ".imp","pid", grep(patterns[i], colnames(complete_wide), value = T))], 
+                    varying=c(4:12),
+                    idvar="pid", 
+                    new.row.names = 1:(nrow(complete) * 9), sep='_', direction='long')
+}
 
-expand <- full_time_invariant[rep(seq_len(nrow(full_time_invariant)), each=9),]
-expand$time <- rep(1:9, times=371)
+full_time_variant<-Reduce(merge, full_time_variant)
 
-###KATIE: WHAT IS THIS DOING##
-##MICE AUTOMATICALLY DROPS VARIABLES AND DOESN'T IMPUTE IF A VARIABLE IS CONSTANT FOR ALL OBSERVATIONS. 
-##SO SINCE PREG_1=1 FOR ALL OBSERVED PARTICIPANTS, IT DIDN'T IMPUTE MISSING VALUES FOR PREG_1, SO I'M DOING THIS MANUALLY
-imp_full<-merge(full_time_variant,expand,by=c("pid",".imp","time"))
+
+full_time_invariant<- complete[, 1:14]
+full_time_invariant <- full_time_invariant[rep(seq_len(nrow(full_time_invariant)), each=9),]
+full_time_invariant$time <- as.factor(rep(1:9, times=371))
+full_time_invariant<-full_time_invariant[,-2] #deletes pointless ".id" column
+
+imp_full<-merge(full_time_variant,full_time_invariant,by=c("pid",".imp","time"))
 imp_full$preg[imp_full$time == 1] <- 1
 imp_full$preg[imp_full$time == 3] <- 0
 imp_full$preg[imp_full$time == 4] <- 0
@@ -213,16 +183,18 @@ row.names(imp_full) <- 1:nrow(imp_full)
 #rename time to visit
 colnames(imp_full)[3] <- "visit"
 
-
-
 ##add back in derived variables
+imp_full$hemo <-as.numeric(imp_full$hemo)
+imp_full$fefood <-as.numeric(imp_full$fefood)
+imp_full$depscore <-as.numeric(imp_full$depscore)
+
 imp_full$anemic <- ifelse((imp_full$preg == 1 & imp_full$hemo < 11) | 
                             (imp_full$preg == 0 & imp_full$hemo < 12),1,0)
 imp_full$gidis_yn <- ifelse((imp_full$nausea == 1 | imp_full$diar == 1),1,0)
 imp_full$fefood_yn <- ifelse((imp_full$fefood > 0),1,0)
 imp_full$pica_child <- ifelse((imp_full$geo_child == 1 | imp_full$amylo_child == 1 
                                | imp_full$other_child == 1),1,0)
-imp_full$pica_yn <- ifelse((imp_full$geo == 1 | imp_full$amylo == 1 | imp_full$other == 1),1,0)
+#imp_full$pica_yn <- ifelse((imp_full$geo == 1 | imp_full$amylo == 1 | imp_full$other == 1),1,0)
 imp_full$visit_2 <- (imp_full$visit)^2
 imp_full$dep_cat <- ifelse((imp_full$depscore > 16),1,0)
  
@@ -245,18 +217,23 @@ mice_final <- rbindlist(mice_complete_list) #using function from data.table
 
 
 
+
 #pull the mice-imputed vars
-add_to_hmi <- mice_final[,c("amylo_child", "fias", "geo_child", "gravidity", "hhloc", "other_child",
+add_to_hmi <- mice_final[,c("pid", ".imp", "amylo_child", "fias", "geo_child", "gravidity", "hhloc", "other_child",
                             "parity", "religion", "site", "tribe", "anemic", "dep_cat", "fefood_yn",
-                            "fi_tert", "gidis_yn", "pica_child", "pica_yn", "wealth")]
+                            "fi_tert", "gidis_yn", "pica_child",  "wealth", "visit")]
+geo_complete$pid <-as.factor(geo_complete$pid)
+add_to_hmi$pid <-as.factor(add_to_hmi$pid)
 
-geo_final<- merge(geo_complete, add_to_hmi,by=c("pid",".imp","visit"))
 
-geo_final<-geo_final[order(geo_final$.imp,geo_final$pid,geo_final$visit),]
-row.names(geo_final)<-1:nrow(geo_final)
+#when all else fails and merge doesn't work
+new_geo_final <- cbind(geo_complete, add_to_hmi)
+new_geo_final <- new_geo_final [, !duplicated(colnames(new_geo_final))]
 
-#some last minute processing
-geo_final<-geo_final[,-c(17,21,37)]
-colnames(geo_final)[15]<-"tribe"
-colnames(geo_final)[30]<-"pica_child"
+#move pid to first column 
+new_geo_final<- new_geo_final[,c(which(colnames(new_geo_final)=="pid"),which(colnames(new_geo_final)!="pid"))]
 
+#delete ".imp"==0 *(orig data)
+new_geo_final<- new_geo_final[which(new_geo_final$.imp !=0), ]
+
+saveRDS(new_geo_final, 'fin_geo.RDS')
